ac62df8292a625e1715a5f21557a2dce28d2b927
Matt Morgan <mattmorgan6@gmail.com>
2021-03-10 12:08:11 -0800

    Add arguments to trixSearch function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 
-function trixSearch() {
+function trixSearch(ixFile: string, wordCount: number, words: Array<string>) {
+
+  console.log(`Recieved ${ixFile} ${wordCount} ${words}`);  
+
   return ["ENST00000445051.1", "ENST00000439876.1"];
 }
+
+
+
 
 
 
@@ -10,6 +16,32 @@ export const search = (searchTerm: string) => {
   if ('development' === process.env.NODE_ENV) {
     console.log(`Search test for ${searchTerm}.`);
   }
-  const r = trixSearch();
+  const ixFile: string = "../test/testData/test1/myTrix.ix";
+  const wordCount: number = 1;
+  const words: Array<string> = ["this"];
+  const r = trixSearch(ixFile, wordCount, words);
   return r;
 };
+
+
+
+
+
+/*
+
+One weakness of C in the string handling.  It is very easy using standard C 
+library functions like sprintf and strcat to write past the end of the
+character array that holds a string.  For this reason instead of sprintf
+we use the "safef" function, which takes an additional parameter, the size
+of the character array.  So
+   char buffer[50];
+   sprintf(buf, "My name is %s", name);
+becomes
+   char buffer[50];
+   safef(buf, sizeof(buf), "My name is %s", name);
+Instead of just silently overflowing the buffer and crashing cryptically
+in many cases if the string is too long, say "Sahar Barjesteh van Waalwijk van Doorn-Khosrovani"
+which is actually a real scientists name!
+
+
+*/
