fe4419dd39b7d7c037ad7536923361b1103a5488
Matt Morgan <mattmorgan6@gmail.com>
2021-03-18 16:18:17 -0700

    Load ixx and set up test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { LocalFile, RemoteFile, BlobFile } from 'generic-filehandle';
 
+type anyFile = LocalFile | RemoteFile | BlobFile;
+
 const trixPrefixSize = 5;
 
-type Trix = {
-  lineFile: LineFile;
-  ixx: TrixIxx; // The position and prefix string
-  ixxSize: number; //
-  ixxAlloc: number;
-  // wordHitHash: Hash;
-  useUdc: boolean;
-};
+// type Trix = {
+//   lineFile: LineFile;
+//   ixx: TrixIxx; // The position and prefix string
+//   ixxSize: number; //
+//   ixxAlloc: number;
+//   // wordHitHash: Hash;
+//   useUdc: boolean;
+// };
 
 // Used as a file description object in Trix.c, not sure if this is exactly needed for ts.
 type LineFile = {
@@ -28,25 +30,59 @@ type TrixIxx = {
 };
 
 // Create a new Trix object and return it.
-function trixNew(): Trix {
-  let lf: LineFile = {
-    filename: '',
-    bufsize: 64 * 1024,
-    buf: '',
-  };
-  let ix: TrixIxx = {
-    pos: 0,
-    prefix: '',
-  };
-  let trix: Trix = {
-    lineFile: lf,
-    ixx: ix,
-    ixxSize: 0,
-    ixxAlloc: 8 * 1024,
-    useUdc: false,
-  };
-  return trix;
+// function trixNew(): Trix {
+//   let lf: LineFile = {
+//     filename: '',
+//     bufsize: 64 * 1024,
+//     buf: '',
+//   };
+//   let ix: TrixIxx = {
+//     pos: 0,
+//     prefix: '',
+//   };
+//   let trix: Trix = {
+//     lineFile: lf,
+//     ixx: ix,
+//     ixxSize: 0,
+//     ixxAlloc: 8 * 1024,
+//     useUdc: false,
+//   };
+//   return trix;
+// }
+
+
+type ParsedIxx = Map<string, number>;
+
+export default class Trix {
+  index: Promise<ParsedIxx>;
+
+  constructor(file: anyFile) {
+    this.index = this._parseIxx(file);
+  }
+
+  // Parses ixx file and returns a ParsedIxx
+  async _parseIxx(file: anyFile): Promise<ParsedIxx> {
+    const ixx = new Map();
+
+    // Load the ixxFile into ixxData object
+    const buf = await file.readFile();
+    const lines = buf.toString('utf8').split('\n');
+    for (const line of lines) {
+      if (line.length > 0) {
+        // Parse the ixx line
+        // Format: 5 characters prefix, 10 characters hex
+        const prefix = line.substr(0, 5);
+        const posStr = line.substr(5);
+        const pos = Number.parseInt(posStr, 16);
+
+        ixx.set(prefix, pos);
+      }
+    }
+
+    return ixx;
+  }
 }
+
 
 // Returns if the given filePath is a url.
 function hasProtocol(urlOrPath: string): boolean {
@@ -54,65 +90,38 @@ function hasProtocol(urlOrPath: string): boolean {
 }
 
 // Open up index.  Load second level index in memory.
-async function trixOpen(ixFile: string) {
-  let trix: Trix = trixNew();
-  trix.useUdc = hasProtocol(ixFile);
-  let ixxFile: string = ixFile + 'x';
-  let lf: LineFile = {
-    filename: ixxFile,
-    bufsize: 64 * 128,
-    buf: '',
-  };
-
-  // Load the ixxFile into ixxData object
-  const local = new LocalFile(ixxFile);
-  // const buf = await local.readFile();
-  const buf = Buffer.alloc(15);
-  const { bytesRead } = await local.read(buf, 0, 15);
-  console.log(buf.toString());
-
-  console.log(buf);
-
-  var Parser = require("@gmod/binary-parser").Parser;
-  var ixxData = new Parser()
-    .string("prefix", {
-      length: 3
-    })
-    .int64("loc");
-
-  // Parse buffer and show result
-  console.log(ixxData.parse(buf));
+async function trixOpen(ixxFile: anyFile) {
+  // let trix: Trix = trixNew();
+  let trix = new Trix(ixxFile);
 }
-
 
 // Search for each word in the words array.
 async function trixSearchCommand(
-  ixFile: string,
+  ixFile: anyFile,
+  ixxFile: anyFile,
   wordCount: number,
   words: Array<string>
 ) {
-  for (let i = 0; i < words.length; i++) 
-    words[i] = words[i].toLowerCase();
+  for (let i = 0; i < words.length; i++) words[i] = words[i].toLowerCase();
 
-  await trixOpen(ixFile);
+  await trixOpen(ixxFile);
 
   return ['ENST00000445051.1', 'ENST00000439876.1'];
 }
-
 
 export const search = async (searchTerm: string) => {
   if ('development' === process.env.NODE_ENV) {
     console.log(`Search test for ${searchTerm}.`);
   }
-  const ixFile: string = './test/testData/test1/myTrix.ix';
+  const filePath: string = './test/testData/test2/out.ix';
+  // const ixFile: string = './test/testData/test1/myTrix.ix';
+  const ixFile = new LocalFile(filePath);
+  const ixxFile = new LocalFile(filePath + 'x');
   const wordCount: number = 1;
   const words: Array<string> = ['This', 'tYme'];
-  const r = await trixSearchCommand(ixFile, wordCount, words);
+  const r = await trixSearchCommand(ixFile, ixxFile, wordCount, words);
   return r;
 };
-
-
-
 
 /*
 Note about safef() function in kent:
--- test/search.test.ts
+++ test/search.test.ts
@@ -1,5 +1,7 @@
 import { search } from '../src/index';
 import { demoParse } from '../src/parse';
+import Trix from '../src/index';
+import { LocalFile } from 'generic-filehandle';
 
 
 const searchTerm: string = "focad";
@@ -9,6 +11,28 @@ describe(`Searching for ${searchTerm}`, () => {
     expect(results).toEqual(["ENST00000445051.1", "ENST00000439876.1"]);
   });
 });
+
+describe(`Test the parsing of test1 ixx file`, () => {
+  it('Parse test1/myTrix.ixx', async () => {
+    const ixxFile: string = './test/testData/test1/myTrix.ixx';
+    const local = new LocalFile(ixxFile);
+    const trix = new Trix(local);
+    const ixx = await trix.index;
+    expect(ixx).toMatchSnapshot();    
+  });
+});
+
+describe(`Test the parsing of test2 ixx file`, () => {
+  it('Parse test2/out.ixx', async () => {
+    const ixxFile: string = './test/testData/test2/out.ixx';
+    const local = new LocalFile(ixxFile);
+    const trix = new Trix(local);
+    const ixx = await trix.index;
+    expect(ixx).toMatchSnapshot();    
+  });
+});
+
+
 
 
 // describe('Demo of the binary parser', () => {
