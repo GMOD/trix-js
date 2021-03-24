d9c8be9ee9a6a964938b21e74ce9e47490c3ae36
Matt Morgan <mattmorgan6@gmail.com>
2021-03-24 11:45:13 -0700

    Add methods to class

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,30 @@ type anyFile = LocalFile | RemoteFile | BlobFile;
 
 const trixPrefixSize = 5;
 
+// TODO:
+//    > convert searchWord to lowercase
+//    > refactor trixSearch()
+//    > update to use trixPrefixSize
+//    > specify maximum number of results as a class variable
+//    > specify minimum number of characters as a class variable?
+
 type ParsedIxx = Map<string, number>;
 
 export default class Trix {
   index: Promise<ParsedIxx>;
+  ixFile: anyFile;
 
-  constructor(file: anyFile) {
-    this.index = this._parseIxx(file);
+  constructor(ixxFile: anyFile, ixFile: anyFile) {
+    this.index = this._parseIxx(ixxFile);
+    this.ixFile = ixFile;
   }
 
   // Parses ixx file and returns a ParsedIxx
-  async _parseIxx(file: anyFile): Promise<ParsedIxx> {
+  async _parseIxx(ixxFile: anyFile): Promise<ParsedIxx> {
     const ixx = new Map();
 
     // Load the ixxFile into ixxData object
-    const buf = await file.readFile();
+    const buf = await ixxFile.readFile();
     const lines = buf.toString('utf8').split('\n');
     for (const line of lines) {
       if (line.length > 0) {
@@ -34,6 +43,119 @@ export default class Trix {
 
     return ixx;
   }
+
+  // Search the .ix file for the searchWord.
+  // Returns list of hit words
+  async trixSearch(searchWord: string) {
+    // 1. Check if searchWord is already in hashTable
+    // 2. Seek ahead to byte `this.index` of `ixFile`
+
+    // For each ixx in trix, compare ixx->prefix and word prefix
+    // If we get a hit, return the position
+
+    // Get position to seek to in .ix file
+    let seekPosStart = 0;
+    let seekPosEnd = -1;
+    const indexes = await this.index;
+    for (let [key, value] of indexes) {
+      if (seekPosEnd === -1) {
+        if (key > searchWord) {
+          seekPosEnd = -2;
+        } else {
+          seekPosStart = value;
+        }
+      } else if (seekPosEnd === -2) {
+        seekPosEnd = -3;
+      } else if (seekPosEnd === -3) {
+        seekPosEnd = value - 1;
+      }
+    }
+
+    let bufLength: number;
+
+    if (seekPosEnd < 0) {
+      // set buf to length of file in bytes - seekPosStart
+      const stat = await this.ixFile.stat();
+      bufLength = stat.size - seekPosStart;
+    } else {
+      bufLength = seekPosEnd - seekPosStart;
+    }
+
+    let buf = Buffer.alloc(bufLength);
+
+    await this.ixFile.read(buf, 0, bufLength, seekPosStart);
+
+    let arr: Array<hit> = [];
+
+    let linePtr = 0;
+
+    // Iterate through the entire buffer
+    while (linePtr < bufLength) {
+      // const lineEndPos = buf.indexOf('\n'.charCodeAt(0));
+
+      let startsWith = true;
+      let done = false;
+
+      let i = linePtr;
+      let cur = String.fromCharCode(buf[i]);
+
+      // 4. Get first word in line and check if it has the same start as searchWord
+      // Iterate through each char of the line in the buffer
+      // break out of loop when we hit a \n
+      while (cur != '\n') {
+        if (i < linePtr + searchWord.length && searchWord[i - linePtr] > cur) {
+          startsWith = false;
+        } else if (
+          i < linePtr + searchWord.length &&
+          searchWord[i - linePtr] < cur
+        ) {
+          if (i < linePtr + 1) done = true;
+          // We are alphabetically ahead so we can break out of the loop
+          else startsWith = false;
+        }
+        i++;
+        cur = String.fromCharCode(buf[i]);
+      }
+
+      if (done) break;
+
+      if (startsWith) {
+        const line: string = buf.slice(linePtr, i).toString();
+
+        arr.push.apply(arr, this._parseHitList(line));
+      }
+
+      // Limit results to 20.
+      if (arr.length >= 20) break;
+
+      linePtr = i + 1;
+    }
+
+    // 5. If it does, get the number of leftoverLetters and add searchWord to hash
+
+    // 6. If it does, return the hitList [list of trixHitPos (itemId: string, wordIx: int, leftoverLetters: int)]
+    return arr;
+  }
+
+  // Takes in a hit string and returns an array of result terms.
+  _parseHitList(line: string) {
+    let arr: Array<hit> = [];
+    const parts = line.split(' ');
+    // Each result is of format: "{itemId},{wordIx}"
+    // Parse the entire line of these and return
+    for (const part of parts) {
+      const pair = part.split(',');
+      if (pair.length == 2) {
+        const itemId: string = pair[0];
+        const wordIx: number = Number.parseInt(pair[1]);
+        const obj: hit = { itemId: itemId, wordIx: wordIx };
+        arr.push(obj);
+      } else if (pair.length > 1) {
+        throw 'Invalid index file.';
+      }
+    }
+    return arr;
+  }
 }
 
 type hit = {
@@ -41,189 +163,7 @@ type hit = {
   wordIx: number;
 };
 
-// TODO: add these as methods to the trix object
-
-// Search the .ix file for the searchWord.
-// Returns list of hit words
-export async function trixSearch(
-  searchWord: string,
-  trix: Trix,
-  ixFile: anyFile
-) {
-  // 1. Check if searchWord is already in hashTable
-  // 2. Seek ahead to byte `trix.index` of `ixFile`
-
-  // For each ixx in trix, compare ixx->prefix and word prefix
-  // If we get a hit, return the position
-
-  // Get position to seek to in .ix file
-  let seekPosStart = 0;
-  let seekPosEnd = -1;
-  const indexes = await trix.index;
-  for (let [key, value] of indexes) {
-    if (seekPosEnd === -1) {
-      if (key > searchWord) {
-        seekPosEnd = -2;
-        // console.log(`${searchWord} < ${key}`)
-      }    
-      else {
-        seekPosStart = value;        
-      }
-    }
-    else if (seekPosEnd === -2) {
-      seekPosEnd = -3;
-    }
-    else if (seekPosEnd === -3) {
-      seekPosEnd = value - 1;
-    }
-  }
-
-  // console.log(`${seekPosStart} ${seekPosEnd}`);
-
-  // TODO: Use buffer with offset to read in from the file.
-
-  let bufLength: number;
-
-  if (seekPosEnd < 0) {
-    // set buf to length of file in bytes - seekPosStart
-    const stat = await ixFile.stat();
-    bufLength = stat.size - seekPosStart;
-  } else {
-    bufLength = seekPosEnd - seekPosStart;
-  }
-
-  let buf = Buffer.alloc(bufLength);
-
-  await ixFile.read(buf, 0, bufLength, seekPosStart);
-
-  // const buf = await ixFile.readFile();
-  // const lines = buf.toString('utf8').split('\n');
-
-  // 3. Loop through each line of `ixFile`
-  // const fs = require('fs');
-  // const stream = fs.createReadStream(ixFile, { highWaterMark: 1000 });
-  // for await(const data of stream) {
-  //     // do something with data
-  //     console.log(data);
-  //     break;
-  // }
-
-  let arr: Array<hit> = [];
-
-  let linePtr = 0;
-
-  // Iterate through the entire buffer
-  while (linePtr < bufLength) {
-    // const lineEndPos = buf.indexOf('\n'.charCodeAt(0));
-
-    let startsWith = true;
-    let done = false;
-
-    let i = linePtr;
-    let cur = String.fromCharCode(buf[i]);
-
-    // 4. Get first word in line and check if it has the same start as searchWord
-    // Iterate through each char of the line in the buffer
-    // break out of loop when we hit a \n
-    while (cur != '\n') {
-      // TODO: subtract linePtr from i
-      if (i < linePtr + searchWord.length && searchWord[i - linePtr] > cur) {
-        startsWith = false;
-      }
-      else if (i < linePtr + searchWord.length && searchWord[i - linePtr] < cur) {
-        if (i < linePtr + 1)
-          done = true;  // We are alphabetically ahead so we can break out of the loop
-        else
-          startsWith = false;
-      }
-      i++;
-      cur = String.fromCharCode(buf[i]);
-    }
-
-    // for  
-    // this
-
-    if (done) break;
-
-    if (startsWith) {
-      const line: string = buf.slice(linePtr, i).toString();
-
-      arr.push.apply(arr, parseHitList(line));
-    }
-
-    // else if (searchWord < line) {
-    //   // console.log(`${searchWord} - ${line}`);
-    //   break;
-    // }
-
-    // Limit results to 20 for now.
-    if (arr.length >= 20) break;
-
-    linePtr = i + 1;
-  }
-
-  // 5. If it does, get the number of leftoverLetters and add searchWord to hash
-
-  // 6. If it does, return the hitList [list of trixHitPos (itemId: string, wordIx: int, leftoverLetters: int)]
-  return arr;
-}
-
-// Takes in a hit string and returns an array of result terms.
-export function parseHitList(line: string) {
-  let arr: Array<hit> = [];
-  const parts = line.split(' ');
-  // Each result is of format: "{itemId},{wordIx}"
-  // Parse the entire line of these and return
-  for (const part of parts) {
-    const pair = part.split(',');
-    if (pair.length == 2) {
-      const itemId: string = pair[0];
-      const wordIx: number = Number.parseInt(pair[1]);
-      const obj: hit = { itemId: itemId, wordIx: wordIx };
-      arr.push(obj);
-    } else if (pair.length > 1) {
-      throw 'Invalid index file.';
-    }
-  }
-  return arr;
-}
-
 // Returns if the given filePath is a url.
 function hasProtocol(urlOrPath: string): boolean {
   return urlOrPath.includes('://');
 }
-
-// Open up index.  Load second level index in memory.
-async function trixOpen(ixxFile: anyFile) {
-  // let trix: Trix = trixNew();
-  let trix = new Trix(ixxFile);
-}
-
-// Search for each word in the words array.
-async function trixSearchCommand(
-  ixFile: anyFile,
-  ixxFile: anyFile,
-  wordCount: number,
-  words: Array<string>
-) {
-  for (let i = 0; i < words.length; i++) words[i] = words[i].toLowerCase();
-
-  await trixOpen(ixxFile);
-
-  return ['ENST00000445051.1', 'ENST00000439876.1'];
-}
-
-export const search = async (searchTerm: string) => {
-  // if ('development' === process.env.NODE_ENV) {
-  //   console.log(`Search test for ${searchTerm}.`);
-  // }
-
-  const filePath: string = './test/testData/test2/out.ix';
-  // const ixFile: string = './test/testData/test1/myTrix.ix';
-  const ixFile = new LocalFile(filePath);
-  const ixxFile = new LocalFile(filePath + 'x');
-  const wordCount: number = 1;
-  const words: Array<string> = ['This', 'tYme'];
-  const r = await trixSearchCommand(ixFile, ixxFile, wordCount, words);
-  return r;
-};
--- test/search.test.ts
+++ test/search.test.ts
@@ -1,23 +1,27 @@
-import { parseHitList, search } from '../src/index';
-import { demoParse } from '../src/parse';
 import Trix from '../src/index';
 import { LocalFile } from 'generic-filehandle';
-import { trixSearch } from '../src/index';
 
+const ixxFile1: string = './test/testData/test1/myTrix.ixx';
+const localIxx1 = new LocalFile(ixxFile1);
+const ixFile1: string = './test/testData/test1/myTrix.ix';
+const localIx1 = new LocalFile(ixFile1);
+const trix1 = new Trix(localIxx1, localIx1);
 
-// describe(`Searching for term.`, () => {
-//   const searchTerm: string = 'focad';
-//   it('Search', async () => {
-//     const results = await search(searchTerm);
-//     expect(results).toEqual(['ENST00000445051.1', 'ENST00000439876.1']);
-//   });
-// });
+const ixxFile2: string = './test/testData/test2/out.ixx';
+const localIxx2 = new LocalFile(ixxFile2);
+const ixFile2: string = './test/testData/test2/out.ix';
+const localIx2 = new LocalFile(ixFile2);
+const trix2 = new Trix(localIxx2, localIx2);
+
+const ixxFile3: string = './test/testData/test3/out.ixx';
+const localIxx3 = new LocalFile(ixxFile3);
+const ixFile3: string = './test/testData/test3/out.ix';
+const localIx3 = new LocalFile(ixFile3);
+const trix3 = new Trix(localIxx3, localIx3);
 
 describe(`Test the parsing of test1 ixx file`, () => {
   it('Parse test1/myTrix.ixx', async () => {
-    const ixxFile: string = './test/testData/test1/myTrix.ixx';
-    const local = new LocalFile(ixxFile);
-    const trix = new Trix(local);
+    const trix = new Trix(localIxx1, localIx1);
     const ixx = await trix.index;
     expect(ixx).toMatchSnapshot();
   });
@@ -25,39 +29,19 @@ describe(`Test the parsing of test1 ixx file`, () => {
 
 describe(`Test the parsing of test2 ixx file`, () => {
   it('Parse test2/out.ixx', async () => {
-    const ixxFile: string = './test/testData/test2/out.ixx';
-    const local = new LocalFile(ixxFile);
-    const trix = new Trix(local);
+    const trix = new Trix(localIxx2, localIx2);
     const ixx = await trix.index;
     expect(ixx).toMatchSnapshot();
   });
 });
 
-describe('Test parsing a hit on test1', () => {
-  it('Parse hit string', async () => {
-    const line = 'this id1,1 id2,1 id3,1';
-    expect(parseHitList(line)).toMatchSnapshot();
-  });
-});
 
-describe('Test parsing a hit on test2', () => {
-  it('Parse hit string', async () => {
-    const line =
-      'abca9-as1 ENST00000458677.1,1 ENST00000627453.2,1 ENST00000627596.2,1 ENST00000627957.2,1 ENST00000629311.1,1 ENST00000630575.2,1 ENST00000630625.1,1';
-    expect(parseHitList(line)).toMatchSnapshot();
-  });
-});
-
-const ixxFile1: string = './test/testData/test1/myTrix.ixx';
-const localIxx1 = new LocalFile(ixxFile1);
-const ixFile1: string = './test/testData/test1/myTrix.ix';
-const localIx1 = new LocalFile(ixFile1);
-const trix1 = new Trix(localIxx1);
+// -----------------------------------------------------------------
 
 describe('Test a search of test1 ix file', () => {
   const searchTerm: string = "this";
   it(`Search for \"${searchTerm}\" in test1/myTrix.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix1, localIx1);
+    const hitList = await trix1.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
@@ -65,7 +49,7 @@ describe('Test a search of test1 ix file', () => {
 describe('Test a search of test1 ix file', () => {
   const searchTerm: string = "is";
   it(`Search for \"${searchTerm}\" in test1/myTrix.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix1, localIx1);
+    const hitList = await trix1.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
@@ -73,7 +57,7 @@ describe('Test a search of test1 ix file', () => {
 describe('Test a search of test1 ix file', () => {
   const searchTerm: string = "id1";
   it(`Search for \"${searchTerm}\" in test1/myTrix.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix1, localIx1);
+    const hitList = await trix1.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
@@ -81,7 +65,7 @@ describe('Test a search of test1 ix file', () => {
 describe('Test a search of test1 ix file', () => {
   const searchTerm: string = "nope";
   it(`Search for \"${searchTerm}\" in test1/myTrix.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix1, localIx1);
+    const hitList = await trix1.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
@@ -89,23 +73,17 @@ describe('Test a search of test1 ix file', () => {
 describe('Test a search of test1 ix file', () => {
   const searchTerm: string = "for";
   it(`Search for \"${searchTerm}\" in test1/myTrix.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix1, localIx1);
+    const hitList = await trix1.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
 
-
-
-const ixxFile2: string = './test/testData/test2/out.ixx';
-const localIxx2 = new LocalFile(ixxFile2);
-const ixFile2: string = './test/testData/test2/out.ix';
-const localIx2 = new LocalFile(ixFile2);
-const trix2 = new Trix(localIxx2);
+// ----------------------------------------------------------------------
 
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "focad";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -114,7 +92,7 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "linc";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -123,7 +101,7 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "none";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -132,7 +110,7 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "prickle";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -141,7 +119,7 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "tim";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -150,7 +128,7 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "znf8";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -159,25 +137,19 @@ describe('Test a search of test2 ix file', () => {
 describe('Test a search of test2 ix file', () => {
   const searchTerm: string = "enst";
   it(`Search for \"${searchTerm}\" in test2/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix2, localIx2);
+    const hitList = await trix2.trixSearch(searchTerm);
     expect(hitList).toMatchSnapshot();
   });
 });
 
 
+// --------------------------------------------------
 
-
-
-const ixxFile3: string = './test/testData/test3/out.ixx';
-const localIxx3 = new LocalFile(ixxFile3);
-const ixFile3: string = './test/testData/test3/out.ix';
-const localIx3 = new LocalFile(ixFile3);
-const trix3 = new Trix(localIxx3);
 
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "focad";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -187,7 +159,7 @@ describe('Test a search of test3 ix file', () => {
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "nothing";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -197,7 +169,7 @@ describe('Test a search of test3 ix file', () => {
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "tim";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -207,7 +179,7 @@ describe('Test a search of test3 ix file', () => {
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "prickle";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -217,7 +189,7 @@ describe('Test a search of test3 ix file', () => {
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "oca";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
@@ -227,7 +199,7 @@ describe('Test a search of test3 ix file', () => {
 describe('Test a search of test3 ix file', () => {
   const searchTerm: string = "zzz";
   it(`Search for \"${searchTerm}\" in test3/out.ix`, async () => {
-    const hitList = await trixSearch(searchTerm, trix3, localIx3);
+    const hitList = await trix3.trixSearch(searchTerm);
     // console.log(hitList);
     expect(hitList).toMatchSnapshot();
   });
