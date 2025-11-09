9d9834eb304411d9e1e83d5610985c94e38077e3
Colin <colin.diesh@gmail.com>
2025-11-09 10:31:17 -0500

    defineConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import importPlugin from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['esm/**/*', 'dist/**/*', '*.js', '*.mjs', 'example/*'],
   },
